d295683405ec10883bb8d3f6ca53491abbcde681
Sergio Diaz <sergiodiaz6085@gmail.com>
2020-08-24 19:59:46 -0500

    [add] Se modifico el app por Home como nueva ruta principal

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 //  Importamos el .jsx que contiene todos los componentes
-import App from './containers/app';
+import App from './routes/app';
 
 //  Aca es donde enviamos el componente que se mostrara en la web
 ReactDom.render(<App />, document.getElementById('app'));
